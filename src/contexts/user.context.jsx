7e0cfeaddd638711915eb61ce2b448fcb84f328b
Christopher Hiew <christopherhiew@protonmail.com>
2023-02-01 15:49:15 +0000

    Initial start to reducer logic

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utilities/firebase/firebase.utils";
 
 
@@ -7,6 +7,19 @@ export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null
 });
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case "SET_CURRENT_USER":
+            return {
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+};
 
 // Thw functional component.
 export const UserProvider = ({ children }) => {
