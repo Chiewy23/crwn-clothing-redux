b593e94686ece13ab8193a7fb7f036565774e8d0
Christopher Hiew <christopherhiew@protonmail.com>
2023-02-21 14:41:39 +0000

    Initial start to new user reducer.

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -2,36 +2,10 @@ import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utilities/firebase/firebase.utils";
 import { createAction } from "../utilities/reducer/reducer.utils";
 
-export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: "SET_CURRENT_USER"
-};
-
-const INITIAL_STATE = {
-    currentUser: null
-};
-
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null
 });
-
-
-
-const userReducer = (state, action) => {
-    const { type, payload } = action;
-
-    switch(type) {
-        case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return {
-                ...state,
-                currentUser: payload
-            }
-        default:
-            throw new Error(`Unhandled type ${type} in userReducer`);
-    }
-};
-
-
 
 export const UserProvider = ({ children }) => {
     const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
