b4c18c1b576e8508ed689273e768559ed4d9c3da
Christopher Hiew <christopherhiew@protonmail.com>
2023-02-21 14:53:55 +0000

    WIP Provider imported in index file.

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import { rootReducer } from './root-reducer';
 import logger from 'redux-logger';
 
 
-
 const middleWares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, middleWares);
+export const store = createStore(rootReducer, undefined, composedEnhancers);
