c357d262f1085de93d9ae2e7c3bc5a35c35a15e9
Christopher Hiew <christopherhiew@protonmail.com>
2023-02-24 15:20:57 +0000

    useSelector for current user.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,15 +4,15 @@ import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utilities/firebase/firebase.utils";
+import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
+import { useSelector } from "react-redux";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
-
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
     const { isCartOpen } = useContext(CartContext);
+    const currentUser = useSelector((state) => state.user.currentUser);
 
     return (
         <Fragment>
