da1d2d79f27dd08779433506056d11d7cd4aedf8
Christopher Hiew <christopherhiew@protonmail.com>
2023-02-28 15:46:22 +0000

    Category selector created.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
@@ -16,11 +15,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
             <CartProvider>
               <App />
             </CartProvider>
-          </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,10 +1,8 @@
-import { Fragment, useContext } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Fragment } from "react";
 
 import CategoryPreview from "../../components/category-preview/category-preview.component";
 
 const CategoriesPreview = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
 
     return (
         <Fragment>
