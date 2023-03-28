53fa7f04874eb7bc27afa2c78686c1c929b0cc91
Christopher Hiew <christopherhiew@protonmail.com>
2023-03-28 15:40:29 +0100

    Cart Icon component updated to use selectors.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
-import { CartContext } from "../../contexts/cart.context";
-
 import { ShoppingIcon, CartItemContainer, ItemCount } from "./cart-icon.styles";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsCartOpen, selectCartCount } from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+    const dispatch = useDispatch();
+    const cartCount = useSelector(selectCartCount);
+    const isCartOpen = useSelector(selectIsCartOpen);
 
     const toggleIsCartOpen = () => {
-        setIsCartOpen(!isCartOpen);
+        dispatch(setIsCartOpen(!isCartOpen));
     };
 
     return (
