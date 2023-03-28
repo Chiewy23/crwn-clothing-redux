e3b1e3ab32eece68efa3c87ed892510ef59650e1
Christopher Hiew <christopherhiew@protonmail.com>
2023-03-28 15:35:47 +0100

    Updated navigation component to use Cart Selector.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,17 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utilities/firebase/firebase.utils";
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../store/user/user.selector";
+import { selectIsCartOpen } from "../../store/cart/cart.selector";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
-    const { isCartOpen } = useContext(CartContext);
+    const isCartOpen = useSelector(selectIsCartOpen)
     const currentUser = useSelector(selectCurrentUser);
 
     return (
