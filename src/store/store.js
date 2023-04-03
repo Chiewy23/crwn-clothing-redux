4594bd26a46245ce82fe750cc9d931eade123982
Christopher Hiew <christopherhiew@protonmail.com>
2023-04-03 15:45:40 +0100

    Implemeting peristor inside of store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,21 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './root-reducer';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
 
 
 const middleWares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
+const persistConfig = {
+    key: "root", //persist everything
+    storage: storage, //use browser localStorage
+    blacklist: ["user"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const persidtor = persistStore(store);
