1d16da7ee99a398b33ae8f52fe1b0a24aea18478
Christopher Hiew <christopherhiew@protonmail.com>
2023-04-03 15:59:53 +0100

    Clearer syntax.

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ import logger from 'redux-logger';
 import storage from 'redux-persist/lib/storage';
 
 
-const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(Boolean);
 
 const persistConfig = {
     key: "root",        // persist everything
